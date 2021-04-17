e2cbab16a838e8a2dae23475cd8b842a0bb975d3
Ian Anderson <ian033190@gmail.com>
2021-04-16 18:36:02 -0700

    Update create_directory.py

--- create_directory.py
+++ create_directory.py
@@ -9,22 +9,24 @@ if len(sys.argv) >2:
     print("Woah there cowbow!\nThis is UNIX!\nLet's avoid spaces in directory names, shall we?\nDon't try to be smart by putting the whole thing in quotes either!")
     exit()
 
-if ' ' in sys.argv[1]:
+if ' ' in maindir:
     print("Okay, look smart guy/gal/non-binary pal.\nThis is UNIX.\nYou REALLY shouldn't be putting spaces in your directory names!!!\nBE BETTER!")
     exit()
  
 current_path = os.path.abspath(os.getcwd())
 
-newpath = os.path.join(current_path, sys.argv[1]) 
+maindir = sys.argv[1]
+
+newpath = os.path.join(current_path, maindir) 
 
 subdirs = ["VCFs","GFFs","misc","data"]
 
-print("Making directory and subdirectories for: {0}".format(sys.argv[1]))
+print("Making directory and subdirectories for: {0}".format(maindir))
 
 for i in subdirs:
     if not os.path.exists(os.path.join(newpath,i)):
         os.makedirs(os.path.join(newpath,i))
     else:
-        print("{0}/{1} already exists.".format(sys.argv[1],i))
+        print("{0}/{1} already exists.".format(maindir,i))
         
 ###
